9b521e73acd9e5f78b3ef0cbd5f39ed9f2a722ab
Digvijay1209 <97652050+Digvijay1209@users.noreply.github.com>
2024-10-20 17:59:34 +0530

    Update jwt.js

--- jwt.js
+++ jwt.js
@@ -1,24 +1,33 @@
 const jwt = require('jsonwebtoken');
 
+require('dotenv').config(); 
+const JWT_SECRET = process.env.JWT_SECRET; 
+
+
+
 const jwtAuthMiddleware = (req, res, next) => {
     const authorization = req.headers.authorization;
-    if (!authorization) return res.status(401).json({ error: 'Token Not Found' });
+
+    if (!authorization)
+        return res.status(401).json({ error: 'Token Not Found' });
 
     const token = req.headers.authorization.split(' ')[1];
-    if (!token) return res.status(401).json({ error: 'Unauthorized' });
+
+    if (!token)
+        return res.status(401).json({ error: 'Unauthorized' });
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, JWT_SECRET);
         req.user = decoded;
         next();
     } catch (err) {
-        console.error(err);
-        res.status(401).json({ error: 'Invalid token' });
+        console.log(err);
+        res.status(401).json({ error: 'Invalid Token' });
     }
-}
+};
 
 const generateToken = (userData) => {
-    return jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: 30000 });
-}
+    return jwt.sign(userData, JWT_SECRET, { expiresIn: 30000 }); 
+};
 
 module.exports = { jwtAuthMiddleware, generateToken };
