fdfe32ee3abe304ba61de854c0f441b4f9f9c64e
Digvijay1209 <97652050+Digvijay1209@users.noreply.github.com>
2024-10-20 18:01:19 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+require('dotenv').config(); 
 
-const mongoURL = process.env.MONGODB_URL_LOCAL;
+const mongoURL = process.env.MONGO_URI;
+
+
+
 mongoose.connect(mongoURL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    
 });
 
 const db = mongoose.connection;
