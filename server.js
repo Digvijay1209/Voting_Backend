13cadeb6a03d54ecad1b3ed0271d575f3176806a
Digvijay1209 <97652050+Digvijay1209@users.noreply.github.com>
2024-10-20 18:01:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const app = express();
+const express=require('express')
+const cors = require('cors'); 
 const db = require('./db');
+
+const app=express();
+app.use(cors());
 require('dotenv').config();
+const bodyParser=require('body-parser');
 
-const bodyParser = require('body-parser'); 
 app.use(bodyParser.json());
-const PORT = process.env.PORT || 3000;
 
-const userRoutes = require('./routes/userRoutes');
+const userRoutes=require('./routes/userRoutes');
 const candidateRoutes = require('./routes/candidateRoutes');
 
-app.use('/user', userRoutes);
-app.use('/candidate', candidateRoutes);
+app.use('/user',userRoutes);
+app.use('/candidate',candidateRoutes);
 
-app.listen(PORT, () => {
-    console.log('listening on port 3000');
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
+    console.log('Hello ');
+})
